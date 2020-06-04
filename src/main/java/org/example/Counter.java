eee731535c8693c959bf75d201e5e2f9486db332
Schachster <anni619@student.sdu.dk>
2020-06-04 22:00:53 +0200

    jUnit testing

--- src/main/java/org/example/Counter.java
+++ src/main/java/org/example/Counter.java
@@ -32,6 +32,14 @@ public class Counter {
         return value;
     }
 
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
     private int value;
 
     public void startIncrement()
